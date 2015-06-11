6dd4a7afe23e28a98ece3c949fa820e8667764c8
suvaneeth <suvaneeth@gmail.com>
2015-06-11 15:12:04 +0300

    Folders n base pages

--- FlyCn/Login.aspx.cs
+++ FlyCn/Login.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace FlyCn
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- FlyCn/Masters/Landing.Master.cs
+++ FlyCn/Masters/Landing.Master.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace FlyCn
+namespace FlyCn.Masters
 {
-    public partial class Contact : Page
+    public partial class Landing : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
