9a8f5b82acf95306714fc64916acf9d64c723165
suvaneeth <suvaneeth@gmail.com>
2015-07-01 11:27:41 +0300

    home page

---
+++ FlyCn/Home.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FlyCn
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadSlider();
+        }
+
+
+        private void LoadSlider()
+        {
+            FlyCnDAL.SliderDataAccess da = new FlyCnDAL.SliderDataAccess();
+            DataSet ds = da.getSliderContent();
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                string img = ds.Tables[0].Rows[i]["ImagePath"].ToString();
+                string Description = ds.Tables[0].Rows[i]["Description"].ToString();
+                string Caption = ds.Tables[0].Rows[i]["Caption"].ToString();
+                string myInnerHtml = " <div>" +
+
+                   " <img" + " u" + "=" + "image" + " src=" + "'" + img + "'" + "/>" +
+                   " <div" + " style" + "=" + "'position: absolute; width: 480px; height: 120px; top: 30px; left: 30px; padding: 5px;text-align: left;" +
+                     "line-height: 60px; text-transform: uppercase; font-size: 40px;color:#FFFFFF;'" + ">" +
+                     Description +
+                   "   </div>" +
+                   " <div style=" + "'position: absolute; width: 480px; height: 120px; top: 300px; left: 30px; padding: 5px;text-align: left; line-height: 36px; font-size: 30px;color:#FFFFFF;'" + ">" +
+                     Caption +
+                   " </div>" +
+                   "</div>";
+                slides.Controls.Add(new LiteralControl(myInnerHtml));
+            }
+        }
+
+    }
+}
---
+++ FlyCn/test1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test1.aspx.cs" Inherits="FlyCn.test1" %>
+
+<%@ Register assembly="Telerik.Web.UI" namespace="Telerik.Web.UI" tagprefix="telerik" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <telerik:RadButton ID="RadButton1" runat="server" Text="RadButton">
+        </telerik:RadButton>
+    </form>
+</body>
+</html>
