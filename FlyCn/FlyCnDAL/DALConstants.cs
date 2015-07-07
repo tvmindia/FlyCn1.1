a7763577d392b497b6e826ddd6f844710f0896d0
suvaneeth <suvaneeth@gmail.com>
2015-07-07 11:50:44 +0300

    cs files added

---
+++ FlyCn/FlyCnDAL/DALConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlyCn.FlyCnDAL
+{
+    public class DALConstants
+    {
+        public string DynamicMasterURL = "../FlyCnMasters/DynamicMaster.aspx";
+        public string ClientPunchListURL = "../EIL/ClientPunchList.aspx";
+        public string ConstructionPunchListURL = "../EIL/ConstructionPunchList.aspx";
+        public string QCPunchListURL = "../EIL/QCPunchList.aspx";
+    }
+}
---
+++ FlyCn/FlyCnDAL/PunchList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telerik.Web.UI;
+
+namespace FlyCn.FlyCnDAL
+{
+    public class PunchList
+    {
+        DALConstants cnst = new DALConstants();
+        public void BindTree(RadTreeView myTree)
+        {
+
+            myTree.Nodes.Clear();
+            RadTreeNode rtn = new RadTreeNode("Construction", "WEIL");
+                rtn.NavigateUrl = cnst.ConstructionPunchListURL + "?Mode=" + rtn.Value;
+                rtn.Target = "contentPane";
+                myTree.Nodes.Add(rtn);
+
+
+                rtn = new RadTreeNode("Client", "CEIL");
+                rtn.NavigateUrl = cnst.ClientPunchListURL + "?Mode=" + rtn.Value;
+                rtn.Target = "contentPane";
+                myTree.Nodes.Add(rtn);
+
+            rtn = new RadTreeNode("QC", "QEIL");
+                rtn.NavigateUrl = cnst.QCPunchListURL + "?Mode=" + rtn.Value;
+                rtn.Target = "contentPane";
+                myTree.Nodes.Add(rtn);
+           
+
+        }
+
+
+    }
+}
