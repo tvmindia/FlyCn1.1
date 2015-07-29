d45f0be074a2b7a1b4658b01ffc8ae57fd192be1
suvaneeth <suvaneeth@gmail.com>
2015-07-29 15:22:56 +0300

    module db added

---
+++ FlyCn/FlyCnDAL/Activities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Telerik.Web.UI;
+using System.Web.UI;
+using System.Data.SqlClient;
+
+namespace FlyCn.FlyCnDAL
+{
+    public class Activities
+    {
+
+
+
+        public void BindTree(RadTreeView myTree)
+        {
+            myTree.Nodes.Clear();
+
+
+            RadTreeNode rtn = new RadTreeNode("Activity Library", "Activity Library"); //<a href="../FlyCnMasters/DynamicMaster.aspx?Mode=Country" target="contentPane">Country</a>
+            rtn.NavigateUrl = "../Activities/ActivityLibrary.aspx";
+                rtn.Target = "contentPane";
+                myTree.Nodes.Add(rtn);
+            
+        }
+
+    }
+}
