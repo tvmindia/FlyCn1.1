cfa14992f493c9902368a6e7e9da6dc5bac3de4c
suvaneeth <suvaneeth@gmail.com>
2015-07-29 15:41:36 +0300

    getmodules

--- FlyCn/FlyCnDAL/Modules.cs
+++ FlyCn/FlyCnDAL/Modules.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.UI;
 
 namespace FlyCn.FlyCnDAL
 {
     public class Modules
     {
+
+
+        public DataSet GetModules()
+        {
+            DataSet dataset = null;
+            SqlConnection con = null;
+            dbConnection dcon = new dbConnection();
+            con = dcon.GetDBConnection();
+
+
+            SqlCommand cmd = new SqlCommand("GetModules", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            dataset = new DataSet();
+            adapter.Fill(dataset);
+            con.Close();
+            return dataset;
+        
+        }
     }
 }
