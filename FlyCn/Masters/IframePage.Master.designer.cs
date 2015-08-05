f02bf075b56d6149f4971a2965b0d21f8251585a
tvm-amrutha <amrutha.sunny@thrithvam.ae>
2015-08-05 11:44:21 +0530

    IframeMaster

--- FlyCn/Masters/IframePage.Master.designer.cs
+++ FlyCn/Masters/IframePage.Master.designer.cs
@@ -22,6 +22,33 @@ namespace FlyCn.Masters {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// Errorbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Errorbox;
+        
+        /// <summary>
+        /// lblErrorInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorInfo;
+        
+        /// <summary>
+        /// masterDiv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl masterDiv;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
