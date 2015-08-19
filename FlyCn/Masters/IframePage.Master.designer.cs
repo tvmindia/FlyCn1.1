3dd8dd9b6ff00a274c46ebbb4b1a1c7d6625a8b1
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-08-19 15:47:13 +0530

    changes in error div

--- FlyCn/Masters/IframePage.Master.designer.cs
+++ FlyCn/Masters/IframePage.Master.designer.cs
@@ -22,24 +22,6 @@ namespace FlyCn.Masters {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// Errorbox control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Errorbox;
-        
-        /// <summary>
-        /// lblErrorInfo control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblErrorInfo;
-        
-        /// <summary>
         /// masterDiv control.
         /// </summary>
         /// <remarks>
@@ -65,5 +47,23 @@ namespace FlyCn.Masters {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Errorbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Errorbox;
+        
+        /// <summary>
+        /// lblErrorInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorInfo;
     }
 }
