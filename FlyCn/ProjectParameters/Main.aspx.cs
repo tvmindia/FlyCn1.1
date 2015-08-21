747e6f5d212795703b9508769e9d960e1d88ea13
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-08-21 14:46:05 +0530

    Created new folder project parameters and added pages Main.aspx and ProjectList.aspx

---
+++ FlyCn/ProjectParameters/Main.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+
+
+namespace FlyCn.ProjectParameters
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadLeftMenu();
+        }
+        public void LoadLeftMenu()
+        {
+            UIClasses.InputPages ip = new UIClasses.InputPages();
+            FlyCnDAL.Activities Activity = new FlyCnDAL.Activities();
+            RadTreeView tview = ip.FindLeftTree(this);
+            Activity.BindTree(tview);
+        }
+    }
+}
---
+++ FlyCn/ProjectParameters/ProjectList.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masters/inputPages.master" AutoEventWireup="true" CodeBehind="ProjectList.aspx.cs" Inherits="FlyCn.ProjectParameters.ProjectList" %>
