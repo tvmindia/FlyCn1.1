2fe420afe5c31e370571d1f1cacc69788f13060d
tvm-amrutha <amrutha.sunny@thrithvam.ae>
2015-08-24 10:55:26 +0530

    tab add edit operation commit

--- FlyCn/UIClasses/TabAddEditSettings.cs
+++ FlyCn/UIClasses/TabAddEditSettings.cs
@@ -8,17 +8,29 @@ namespace FlyCn.UIClasses
 {
     public class TabAddEditSettings
     {
-        public void Addtab()
+        public void Addtab(RadTab tabid1, RadTab tabid2)
         {
+            tabid1.Selected = true;
+            tabid2.Text = "New";
+            tabid2.ImageUrl = "~/Images/Icons/NewIcon.png";
 
         }
-        public void EditTab(int tabid)
+        public void EditTab(RadTab tabid)
         {
             //RadTab tab = (RadTab)RadTabStrip1.FindTabByValue("2");
-            //tab.Selected = true;
-            //tab.Text = "Edit";
-            //tab.ImageUrl = "~/Images/Icons/editIcon.png";
-                
+            tabid.Selected = true;
+            tabid.Text = "Edit";
+            tabid.ImageUrl = "~/Images/Icons/editIcon.png";
+         
+        }
+
+        public void ListTab(RadTab tabid1, RadTab tabid2)
+        {
+            //RadTab tab = (RadTab)RadTabStrip1.FindTabByValue("2");
+            tabid1.Selected = true;
+            tabid2.Text = "New";
+            tabid2.ImageUrl = "~/Images/Icons/NewIcon.png";
+
         }
     }
 }
