f15e74edec98b873516c8243ff06ce35704c0d4e
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-09-10 16:09:29 +0530

    update function added

--- FlyCn/ProjectParameters/ManageProject.aspx.cs
+++ FlyCn/ProjectParameters/ManageProject.aspx.cs
@@ -9,9 +9,11 @@ using FlyCn.FlyCnDAL;
 
 namespace FlyCn.ProjectParameters
 {
+
     public partial class ManageProject : System.Web.UI.Page
     {
         FlyCnDAL.ProjectParameters pObj = new FlyCnDAL.ProjectParameters();
+        ErrorHandling eObj = new ErrorHandling();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -19,15 +21,16 @@ namespace FlyCn.ProjectParameters
         #region ToolBar_OnClick
         protected void ToolBar_onClick(object sender, Telerik.Web.UI.RadToolBarEventArgs e)
         {
-        
-            //if (e.Item.Value == "Update")
-            //{
-               
 
-            //}
-           
+            if (e.Item.Value == "Update")
+            {
+
+                Update();
+            }
+
         }
         #endregion ToolBar_OnClick
+
         #region RadGrid1_NeedDataSource1
         protected void RadGrid1_NeedDataSource1(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
         {
@@ -42,23 +45,23 @@ namespace FlyCn.ProjectParameters
 
         protected void RadGrid1_ItemCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
         {
-            if(e.CommandName=="EditData")
+            if (e.CommandName == "EditData")
             {
                 string projno = e.Item.OwnerTableView.DataKeyValues[e.Item.ItemIndex]["ProjectNo"].ToString();
                 DataTable dt;
                 dt = pObj.GetProjectParameters(projno);
                 txtProjName.Text = dt.Rows[0]["ProjectName"].ToString();
-                txtProjNo.Text=dt.Rows[0]["ProjectNo"].ToString();
+                txtProjNo.Text = dt.Rows[0]["ProjectNo"].ToString();
                 txtLocation.Text = dt.Rows[0]["ProjectLocation"].ToString();
                 txtManager.Text = dt.Rows[0]["Project_Manager"].ToString();
                 txtCompanyName.Text = dt.Rows[0]["CompName"].ToString();
-                txtAddress1.Text=dt.Rows[0]["CompAdd1"].ToString();
+                txtAddress1.Text = dt.Rows[0]["CompAdd1"].ToString();
                 txtAddress2.Text = dt.Rows[0]["CompAdd2"].ToString();
                 txtTelephone.Text = dt.Rows[0]["CompTeleNo"].ToString();
                 txtFax.Text = dt.Rows[0]["CompFaxNo"].ToString();
                 txtEmail.Text = dt.Rows[0]["CompEmailAdd"].ToString();
                 txtWebsite.Text = dt.Rows[0]["CompWebSite"].ToString();
-                txtClientName.Text = dt.Rows[0]["ClientName"].ToString();
+                //txtClientName.Text = dt.Rows[0]["ClientName"].ToString();
                 txtContractDetails.Text = dt.Rows[0]["ContractNo"].ToString();
                 //txtClientTelephone.Text = dt.Rows[0][""].ToString();
                 // txtClientFax.Text = dt.Rows[0][""].ToString();
@@ -67,8 +70,8 @@ namespace FlyCn.ProjectParameters
                 txtImplementation.Text = dt.Rows[0]["Regional_ImplEngineer"].ToString();
                 txtProjectAdmin.Text = dt.Rows[0]["Project_Administrator"].ToString();
                 //txtPunchList.Text = dt.Rows[0][""].ToString();
-               // txtPunchListPerson.Text = dt.Rows[0][""].ToString();
-               // txtPunchListToCompany.Text = dt.Rows[0][""].ToString();
+                // txtPunchListPerson.Text = dt.Rows[0][""].ToString();
+                // txtPunchListToCompany.Text = dt.Rows[0][""].ToString();
                 //txtPunchListToPerson.Text = dt.Rows[0][""].ToString();
                 txtPlant.Text = dt.Rows[0]["Plant_Caption"].ToString();
                 txtArea.Text = dt.Rows[0]["Area_Caption"].ToString();
@@ -82,11 +85,57 @@ namespace FlyCn.ProjectParameters
                 txtPaymentCurrency.Text = dt.Rows[0]["PaymentCurrency"].ToString();
                 txtLunchBreakMinutes.Text = dt.Rows[0]["LunchBreak_Minutes"].ToString();
                 txtLevel1.Text = dt.Rows[0]["SchCaptionLevel1"].ToString();
-                txtLevel2.Text = dt.Rows[0]["SchCaptionLevel2"].ToString();                                             
+                txtLevel2.Text = dt.Rows[0]["SchCaptionLevel2"].ToString();
                 txtLevel3.Text = dt.Rows[0]["SchCaptionLevel3"].ToString();
                 txtClient1.Text = dt.Rows[0]["Weld_Client1Caption"].ToString();
                 txtClient2.Text = dt.Rows[0]["Weld_Client2Caption"].ToString();
                 txtThirdParty.Text = dt.Rows[0]["Weld_ThirdPartyCaption"].ToString();
+            }
+        }
+        public void Update()
+        {
+            try
+            {
+                int result = 0;
+                pObj.ProjectNum = txtProjNo.Text;
+                pObj.ProjectName = txtProjName.Text;
+                pObj.Location_Caption = txtLocation.Text;
+                pObj.Project_Manager = txtManager.Text;
+                pObj.CompName = txtCompanyName.Text;
+                pObj.CompAdd1 = txtAddress1.Text;
+                pObj.CompAdd2 = txtAddress2.Text;
+                pObj.CompTeleNo = txtTelephone.Text;
+                pObj.CompFaxNo = txtFax.Text;
+                pObj.CompEmailAdd = txtEmail.Text;
+                pObj.CompWebSite = txtWebsite.Text;
+                pObj.ClientName = txtClientName.Text;
+                pObj.ContractNo = txtContractDetails.Text;
+                pObj.Regional_ImplEngineer = txtImplementation.Text;
+                pObj.Project_Administrator = txtProjectAdmin.Text;
+                pObj.Plant_Caption = txtPlant.Text;
+                pObj.Area_Caption = txtArea.Text;
+                pObj.Location_Caption = txtLocation.Text;
+                pObj.TO_System_Caption = txtSystem.Text;
+                pObj.TO_SubSystem_Caption = txtsubsystem.Text;
+                pObj.MiscManpowerTracking_Caption = txtManager.Text;
+                pObj.CaptionForOtherCost1 = txtOtherCost1.Text;
+                pObj.CaptionForOtherCost2 = txtOtherCost2.Text;
+                pObj.CaptionForOtherCost3 = txtOtherCost3.Text;
+                pObj.PaymentCurrency = txtPaymentCurrency.Text;
+                pObj.LunchBreak_Minutes = Convert.ToDecimal(txtLunchBreakMinutes.Text);
+                pObj.SchCaptionLevel1 = txtLevel1.Text;
+                pObj.SchCaptionLevel2 = txtLevel2.Text;
+                pObj.SchCaptionLevel3 = txtLevel3.Text;
+                pObj.Weld_Client1Caption = txtClient1.Text;
+                pObj.Weld_Client2Caption = txtClient2.Text;
+                pObj.Weld_ThirdPartyCaption = txtThirdParty.Text;
+                result = pObj.EditProjectParameters(pObj.ProjectNum);
+            }
+            catch (Exception ex)
+            {
+                var page = HttpContext.Current.CurrentHandler as Page;
+                var master = page.Master;
+                eObj.ErrorData(ex, page);
             }
         }
     }
