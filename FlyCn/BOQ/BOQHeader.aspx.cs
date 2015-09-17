96fcc7d213ced7ceeaa8eabafdfa19e00dafbaf2
tvm-albert <albert.thomson@thrithvam.ae>
2015-09-17 12:52:27 +0530

    Created document and revision classes

--- FlyCn/BOQ/BOQHeader.aspx.cs
+++ FlyCn/BOQ/BOQHeader.aspx.cs
@@ -10,8 +10,12 @@ namespace FlyCn.BOQ
 {
     public partial class BOQHeader : System.Web.UI.Page
     {
+        UIClasses.Const Const = new UIClasses.Const();
+        FlyCnDAL.Security.UserAuthendication UA;
         protected void Page_Load(object sender, EventArgs e)
         {
+            UA = (FlyCnDAL.Security.UserAuthendication)Session[Const.LoginSession];
+            string ding = UA.projectNo;
             //txtDocumentno.Text = "--no--";
         }
         protected void RadGrid1_NeedDataSource(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
--- FlyCn/FlyCnDAL/BOQ.cs
+++ FlyCn/FlyCnDAL/BOQ.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using Telerik.Web.UI;
@@ -8,14 +9,212 @@ namespace FlyCn.FlyCnDAL
 {
     public class BOQ
     {
-       public void BindTree(RadTreeView myTree)
-       {
+    #region Billofquantityproperties
+
+         #region Boqheaderproperty
+        public string RevisionNo
+        {
+            get;
+            set;
+        }
+        public DateTime DocumentDate
+        {
+            get;
+            set;
+        }
+        public string DocumentTitle
+        {
+            get;
+            set;
+        }
+        public string Remarks
+        {
+            get;
+            set;
+        }
+
+        #endregion Boqheaderproperty
+        #region Boqdetailproperty
+        public int ItemNo
+        {
+            get;
+            set;
+        }
+        public string ItemDescription
+        {
+            get;
+            set;
+        }
+        public Double Quantity
+        {
+            get;
+            set;
+        }
+        public string Unit
+        {
+            get;
+            set;
+        }
+        public Double NormHours
+        {
+            get;
+            set;
+        }
+        public Double LabourRate
+        {
+            get;
+            set;
+        }
+        public string LabourRateType
+        {
+            get;
+            set;
+
+        }
+        public Double MaterialRate
+        {
+            get;
+            set;
+        }
+        public Double UDFRate1
+        {
+            get;
+            set;
+        }
+        public string UDFRateType1
+        {
+            get;
+            set;
+        }
+        public Double UDFRate2
+        {
+            get;
+            set;
+        }
+        public string UDFRateType2
+        {
+            get;
+            set;
+        }
+        public Double UDFRate3
+        {
+            get;
+            set;
+        }
+        public string UDFRateType3
+        {
+            get;
+            set;
+        }
+        public Double UDFRate4
+        {
+            get;
+            set;
+        }
+        public string UDFRateType4
+        {
+            get;
+            set;
+        }
+        public Double UDFRate5
+        {
+            get;
+            set;
+        }
+        public string UDFRateType5
+        {
+            get;
+            set;
+        }
+        public string Group1
+        {
+            get;
+            set;
+        }
+        public string Group2
+        {
+            get;
+            set;
+        }
+        public string Group3
+        {
+            get;
+            set;
+        }
+        public string Group4
+        {
+            get;
+            set;
+        }
+        public string Group5
+        {
+            get;
+            set;
+        }
+     #endregion Boqdetailproperty
+
+    #endregion Billofquantityproperties
+
+
+
+        #region Billofquantitymethods
+        public void BindTree(RadTreeView myTree)
+        {
            myTree.Nodes.Clear();
-       }
-       public void LoadInputScreen(RadPane myContentPane)
-       {
+        }
+        public void LoadInputScreen(RadPane myContentPane)
+        {
            myContentPane.ContentUrl = "BOQHeader.aspx";
-       }
-        
+        }
+        /// <summary>
+        /// inserting Boq header and details
+        /// </summary>
+        public void Insertboq()
+        {
+            try
+            {
+                SqlConnection con = null;
+                dbConnection dcon = new dbConnection();
+                con = dcon.GetDBConnection();
+                SqlCommand boqcmd = new SqlCommand();
+                boqcmd.Connection = con;
+                boqcmd.CommandType = System.Data.CommandType.StoredProcedure;
+                boqcmd.CommandText = "";
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+                boqcmd.Parameters.Add("");
+
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+            }
+        }
+
+
+        #endregion Billofquantitymethods
+
     }
+       
 }
