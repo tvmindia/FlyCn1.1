7cfafed35f8dc564656d0426e3edb58fbdf3c8e9
suvaneeth <suvaneeth@gmail.com>
2015-09-28 14:08:11 +0300

    RedTheme

--- FlyCn/Default.aspx
+++ FlyCn/Default.aspx
@@ -1,8 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masters/Landing.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FlyCn.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainBody" runat="server">
-    <iframe src="General/UnderConstruction.aspx" class="fullScreenIframe">
-
-    </iframe>
+    
 </asp:Content>
--- FlyCn/Templates/InputTemplateContent.aspx.designer.cs
+++ FlyCn/Templates/InputTemplateContent.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace FlyCn.Templates {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// RadTabStrip1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadTabStrip RadTabStrip1;
+        
+        /// <summary>
         /// ToolBar control.
         /// </summary>
         /// <remarks>
