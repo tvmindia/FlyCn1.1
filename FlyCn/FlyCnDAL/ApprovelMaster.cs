7f6595d39d69732fdba099be4756226cb43ef979
tvm-amrutha <amrutha.sunny@thrithvam.ae>
2015-10-08 16:15:38 +0530

    add new method insert approvelmaster

--- FlyCn/FlyCnDAL/ApprovelMaster.cs
+++ FlyCn/FlyCnDAL/ApprovelMaster.cs
@@ -10,6 +10,54 @@ using System.Web;
     public class ApprovelMaster
     {
 
+
+        public Guid RevisionID
+        {
+            get;
+            set;
+        }
+        public Guid ApprovalID
+        {
+            get;
+            set;
+        }
+
+        public Guid DocumentID
+        {
+            get;
+            set;
+        }
+        public Guid VerifierID
+        {
+            get;
+            set;
+        }
+        public int VerifierLevel
+        {
+            get;
+            set;
+        }
+        public int ApprovalStatus
+        {
+            get;
+            set;
+        }
+        public DateTime ApprovalDate
+        {
+            get;
+            set;
+        }
+     
+        public string Remarks
+        {
+            get;
+            set;
+        }
+        public string CreatedBy
+        {
+            get;
+            set;
+        }
         public DataTable getDataFromApprovelLevel()
         {
             DataTable datatableobj = null;
@@ -38,6 +86,34 @@ using System.Web;
             cmd.Parameters.AddWithValue("@Level", Level);
             cmd.Parameters.AddWithValue("@DocumentType", documentType);
             cmd.Parameters.AddWithValue("@ProjectNo", projectNo);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            datatableobj = new DataTable();
+            adapter.Fill(datatableobj);
+            con.Close();
+            return datatableobj;
+        }
+
+
+
+
+        public DataTable InsertApprovelMaster()
+        {
+            DataTable datatableobj = null;
+            SqlConnection con = null;
+            dbConnection dcon = new dbConnection();
+            con = dcon.GetDBConnection();
+            SqlCommand cmd = new SqlCommand("InsertApprovelMaster", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ApprovalID", ApprovalID);
+            cmd.Parameters.AddWithValue("@DocumentID", DocumentID);
+            cmd.Parameters.AddWithValue("@RevisionID", RevisionID);
+            cmd.Parameters.AddWithValue("@VerifierID", VerifierID);
+            cmd.Parameters.AddWithValue("@VerifierLevel", VerifierLevel);
+            cmd.Parameters.AddWithValue("@ApprovalStatus", ApprovalStatus);
+            cmd.Parameters.AddWithValue("@ApprovalDate", ApprovalDate);
+            cmd.Parameters.AddWithValue("@Remarks", Remarks);
+            cmd.Parameters.AddWithValue("@CreatedBy", CreatedBy);
             SqlDataAdapter adapter = new SqlDataAdapter();
             adapter.SelectCommand = cmd;
             datatableobj = new DataTable();
