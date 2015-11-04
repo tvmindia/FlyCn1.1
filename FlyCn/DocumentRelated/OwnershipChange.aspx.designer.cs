33066f4a3fcd2aa31cffe16d9c36ee8cc03f744a
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-11-04 12:40:58 +0530

    ChangeOwnership method calling

--- FlyCn/DocumentRelated/OwnershipChange.aspx.designer.cs
+++ FlyCn/DocumentRelated/OwnershipChange.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace FlyCn.DocumentRelated {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtRemarks;
+        
+        /// <summary>
+        /// btnAcquire control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAcquire;
     }
 }
