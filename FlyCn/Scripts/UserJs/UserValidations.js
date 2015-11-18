428cec694fd4ddd5b470d16a4df491eb2c333a6d
tvm-albert <albert.thomson@thrithvam.ae>
2015-11-18 15:56:45 +0530

    added new folder userjs in scripts folder

---
+++ FlyCn/Scripts/UserJs/UserValidations.js
@@ -0,0 +1,11 @@
+﻿/*Validation Methods are defined here*/
+
+
+function IsNumeric(e) {
+    var specialKeys = new Array();
+    specialKeys.push(8); //Backspace
+    specialKeys.push(46);//dot operator
+    var keyCode = e.which ? e.which : e.keyCode
+    var ret = ((keyCode >= 48 && keyCode <= 57) || specialKeys.indexOf(keyCode) != -1);//ascii key checking
+    return ret;
+}
