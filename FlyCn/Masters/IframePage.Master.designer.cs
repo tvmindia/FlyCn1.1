525effbc77c58537eb641aec84e41d044ecb4bb2
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-11-30 14:01:30 +0530

    Changes

--- FlyCn/Masters/IframePage.Master.designer.cs
+++ FlyCn/Masters/IframePage.Master.designer.cs
@@ -92,5 +92,23 @@ namespace FlyCn.Masters {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HiddenField HiddenContentPage;
+        
+        /// <summary>
+        /// hdnSecuritySubpage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnSecuritySubpage;
+        
+        /// <summary>
+        /// hdnSecurityMaster control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnSecurityMaster;
     }
 }
