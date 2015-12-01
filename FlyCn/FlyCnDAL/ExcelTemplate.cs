e3c45963929822092896354a6c55a8bff049e54b
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-12-01 15:59:10 +0530

    Excel template

--- FlyCn/FlyCnDAL/ExcelTemplate.cs
+++ FlyCn/FlyCnDAL/ExcelTemplate.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,37 @@ namespace FlyCn.FlyCnDAL
 {
     public class ExcelTemplate
     {
+        public void GenerateExcelTemplate(string projno,string tablename)
+        {
+
+            DataTable dt = new DataTable();
+            SqlConnection con = null;
+            SqlDataAdapter daObj;
+            try
+            {
+                dbConnection dcon = new dbConnection();
+                con = dcon.GetDBConnection();
+                UIClasses.Const Const = new UIClasses.Const();
+                FlyCnDAL.Security.UserAuthendication UA;
+                HttpContext context = HttpContext.Current;
+                UA = (FlyCnDAL.Security.UserAuthendication)context.Session[Const.LoginSession];
+                string selectQuery = "procSelectFieldsFromSYS_TableDefinitionByTable_NameProjectNo";
+                SqlCommand cmdSelect = new SqlCommand(selectQuery, con);
+                cmdSelect.CommandType = CommandType.StoredProcedure;
+                cmdSelect.Parameters.AddWithValue("@projno", projno);
+                cmdSelect.Parameters.AddWithValue("@tablename", tablename);
+                daObj = new SqlDataAdapter(cmdSelect);
+                daObj.Fill(dt);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
     }
 }
