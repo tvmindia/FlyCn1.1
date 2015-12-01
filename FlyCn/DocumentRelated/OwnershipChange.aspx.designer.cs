117fee25eb52053c07f7b21db401bc4f8ff37f28
tvm-gopika <gopika.vipin@thrithvam.ae>
2015-12-01 12:11:57 +0530

    Added new class for excel template

--- FlyCn/DocumentRelated/OwnershipChange.aspx.designer.cs
+++ FlyCn/DocumentRelated/OwnershipChange.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace FlyCn.DocumentRelated {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HiddenField popuprefreshRequired;
+        
+        /// <summary>
+        /// hiddenCloseFlag control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hiddenCloseFlag;
     }
 }
