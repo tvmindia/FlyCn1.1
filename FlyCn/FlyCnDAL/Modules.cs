276156f75ec27019acd3730d14e8087acef6b8d2
tvm-albert <albert.thomson@thrithvam.ae>
2015-12-03 14:16:59 +0530

    modules new function addded

--- FlyCn/FlyCnDAL/Modules.cs
+++ FlyCn/FlyCnDAL/Modules.cs
@@ -8,15 +8,18 @@ using System.Web.UI;
 
 namespace FlyCn.FlyCnDAL
 {
+
     public class Modules
     {
         ErrorHandling eObj = new ErrorHandling();
-        DataSet dataset = null;
+        DataSet ds = null;
         SqlConnection con = null;
         //previous
         public string cmbTextField = "ModuleID";
         public string cmbValueField = "ModuleDesc";
         //previous
+
+        #region ModulesProperties
 
         public string ModuleID
         {
@@ -93,7 +96,11 @@ namespace FlyCn.FlyCnDAL
             set;
         }
 
+        #endregion ModulesProperties
 
+
+        #region ModulesMethods
+        #region GetModules
         public DataSet GetModules()
         {
             try
@@ -104,8 +111,8 @@ namespace FlyCn.FlyCnDAL
                 cmd.CommandType = CommandType.StoredProcedure;
                 SqlDataAdapter adapter = new SqlDataAdapter();
                 adapter.SelectCommand = cmd;
-                dataset = new DataSet();
-                adapter.Fill(dataset);
+                ds = new DataSet();
+                adapter.Fill(ds);
             }
             catch(Exception ex)
             {
@@ -120,13 +127,64 @@ namespace FlyCn.FlyCnDAL
                     con.Close();
                 }
             }
+            return ds;
+        }
+        #endregion GetModules
 
+        #region GetModuleByModuleID
+        public void GetModule(string moduleID)
+        {
+            try
+            {
+                dbConnection dcon = new dbConnection();
+                con = dcon.GetDBConnection();
+                SqlCommand cmd = new SqlCommand("GetModuleByModuleID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@ModuleID", SqlDbType.NVarChar, 10).Value = moduleID;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                ds = new DataSet();
+                adapter.Fill(ds);
+
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    ModuleID = ds.Tables[0].Rows[0]["ModuleID"].ToString();
+                    ModuleDesc = ds.Tables[0].Rows[0]["ModuleDesc"].ToString();
+                    ModuleType = ds.Tables[0].Rows[0]["ModuleType"].ToString();
+                    CreateCategory_YN = ds.Tables[0].Rows[0]["CreateCategory_YN"].ToString();
+                    BaseTable=ds.Tables[0].Rows[0]["BaseTable"].ToString();
+                    TrackingTable=ds.Tables[0].Rows[0]["TrackingTable"].ToString();
+                    ModuleUniqueKey= ds.Tables[0].Rows[0]["ModuleUniqueKey"].ToString();
+                    ModuleWebForm=ds.Tables[0].Rows[0]["ModuleWebForm"].ToString();
+                    MenuLevel_1_Page=ds.Tables[0].Rows[0]["MenuLevel_1_Page"].ToString();
+                    ReportModuleLink=ds.Tables[0].Rows[0]["ReportModuleLink"].ToString();
+                    int disorder;
+                    int.TryParse(ds.Tables[0].Rows[0]["DisplayOrder"].ToString(),out disorder);
+                    DisplayOrder=disorder;
+                    ModuleIconURL=ds.Tables[0].Rows[0]["ModuleIconURL"].ToString();
+                    ModuleIconURLsmall=ds.Tables[0].Rows[0]["ModuleIconURLsmall"].ToString();
+                    IS_ALLOWED_FOR_SCOPE_DEFN=ds.Tables[0].Rows[0]["IS_ALLOWED_FOR_SCOPE_DEFN"].ToString();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                var page = HttpContext.Current.CurrentHandler as Page;
+                eObj.ErrorData(ex, page);
+                throw ex;
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
           
-            return dataset;
-       
         }
 
-
+        #endregion GetModuleByModuleID
+        #endregion ModulesMethods
 
 
 
