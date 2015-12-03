e5eb4519bd3a255e6730797b5470528fd576d6ca
tvm-albert <albert.thomson@thrithvam.ae>
2015-12-03 14:36:47 +0530

    table def functions added to commondal and modules tbl functio removed

--- FlyCn/FlyCnDAL/CommonDAL.cs
+++ FlyCn/FlyCnDAL/CommonDAL.cs
@@ -103,7 +103,11 @@ namespace FlyCn.FlyCnDAL
             get;
             set;
         }
-
+        public string ImportProperty
+        {
+            get;
+            set;
+        }
         #endregion  Public Properties
 
             ErrorHandling eObj = new ErrorHandling();
@@ -373,6 +377,90 @@ namespace FlyCn.FlyCnDAL
             }
 
             #endregion Get Where Condition 
+
+
+            #region Method to get table definition
+            /// <summary>
+            /// Get The Table Definition
+            /// </summary>
+            /// <param name="TableName"></param>
+            /// <returns>Dataset</returns>
+            public DataSet getTableDefinition(string TableName)
+            {
+
+                SqlConnection con = new SqlConnection();
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet myRec = new DataSet();
+
+                dbConnection dcon = new dbConnection();
+                con = dcon.GetDBConnection();
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SelectTable";
+                cmd.Parameters.Add("@tablename", SqlDbType.NVarChar).Value = TableName;
+                cmd.Connection = con;
+                da.SelectCommand = cmd;
+
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    da.Fill(myRec);
+                    return myRec;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+            }
+            #endregion Method to get table definition
+
+            #region Method to get Procedure Name
+            /// <summary>
+            /// Get The Procedure Name From DataBase
+            /// </summary>
+            /// <param name="TableName"></param>
+            /// <returns>Procedure Name</returns>
+            public string getProcedureName(string TableName)
+            {
+                string procName = "";
+                SqlConnection con = new SqlConnection();
+                SqlCommand cmd = new SqlCommand();
+
+                dbConnection dcon = new dbConnection();
+                con = dcon.GetDBConnection();
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "GetProcedureName";
+                cmd.Parameters.Add("@tablename", SqlDbType.NVarChar).Value = TableName;
+                cmd.Parameters.Add("@propertyName", SqlDbType.NVarChar).Value = ImportProperty;
+                cmd.Connection = con;
+
+                try
+                {
+                    con.Open();
+                    procName = cmd.ExecuteScalar().ToString();
+                    return procName;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+            }
+            #endregion Method to get Procedure Name
 
             #endregion CommonDALMethods
     }
