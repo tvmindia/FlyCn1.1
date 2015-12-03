c5e49326e285828c4cf490fa895829cc5996d9ee
tvm-albert <albert.thomson@thrithvam.ae>
2015-12-03 11:52:41 +0530

    modules.cs  function set and property seting

--- FlyCn/FlyCnDAL/Modules.cs
+++ FlyCn/FlyCnDAL/Modules.cs
@@ -10,25 +10,118 @@ namespace FlyCn.FlyCnDAL
 {
     public class Modules
     {
+        ErrorHandling eObj = new ErrorHandling();
+        DataSet dataset = null;
+        SqlConnection con = null;
+        //previous
         public string cmbTextField = "ModuleID";
         public string cmbValueField = "ModuleDesc";
+        //previous
+
+        public string ModuleID
+        {
+            get;
+            set;
+        }
+
+        public string ModuleDesc
+        {
+            get;
+            set;
+        }
+
+        public string ModuleType//nvarchar(1) type
+        {
+            get;
+            set;
+        }
+        public string CreateCategory_YN//nvarchar(1) type
+        {
+            get;
+            set;
+        }
+        public string BaseTable
+        {
+            get;
+            set;
+        }
+        public string TrackingTable
+        {
+            get;
+            set;
+        }
+        public string ModuleUniqueKey
+        {
+            get;
+            set;
+        }
+        public string ModuleWebForm
+        {
+            get;
+            set;
+        }
+
+        public string MenuLevel_1_Page
+        {
+            get;
+            set;
+        }
+        public string ReportModuleLink
+        {
+            get;
+            set;
+        }
+        public int DisplayOrder
+        {
+            get;
+            set;
+        }
+        public string ModuleIconURL
+        {
+            get;
+            set;
+        }
+        public string ModuleIconURLsmall
+        {
+            get;
+            set;
+        }
+
+        public string IS_ALLOWED_FOR_SCOPE_DEFN//nvarchar(1)
+        {
+            get;
+            set;
+        }
+
 
         public DataSet GetModules()
         {
-            DataSet dataset = null;
-            SqlConnection con = null;
-            dbConnection dcon = new dbConnection();
-            con = dcon.GetDBConnection();
+            try
+            {
+                dbConnection dcon = new dbConnection();
+                con = dcon.GetDBConnection();
+                SqlCommand cmd = new SqlCommand("GetAllModules", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                dataset = new DataSet();
+                adapter.Fill(dataset);
+            }
+            catch(Exception ex)
+            {
+                var page = HttpContext.Current.CurrentHandler as Page;
+                eObj.ErrorData(ex, page);
+                throw ex;
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
 
-
-            SqlCommand cmd = new SqlCommand("GetAllModules", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            adapter.SelectCommand = cmd;
-            dataset = new DataSet();
-            adapter.Fill(dataset);
-            con.Close();
+          
             return dataset;
        
         }
