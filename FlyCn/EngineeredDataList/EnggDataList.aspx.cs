d7b4e9f88ab2f1bf35f02bc462d46eeb04d33731
tvm-amrutha <amrutha.sunny@thrithvam.ae>
2015-12-03 11:39:05 +0530

    add a new class

--- FlyCn/EngineeredDataList/EnggDataList.aspx.cs
+++ FlyCn/EngineeredDataList/EnggDataList.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
 
 namespace FlyCn.EngineeredDataList
 {
@@ -11,7 +12,16 @@ namespace FlyCn.EngineeredDataList
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+              UIClasses.InputPages ip = new UIClasses.InputPages();
+              FlyCnDAL.EnggDataList objBOQ = new FlyCnDAL.EnggDataList();
 
+            RadTreeView tview = ip.FindLeftTree(this);
+            //tview.Attributes.Add("onclick", "ClientNodeClicked(event)");
+           
+            objBOQ.BindTree(tview);
+
+            RadPane radpane = ip.FindContentPane(this);
+            objBOQ.LoadInputScreen(radpane);
         }
     }
 }
