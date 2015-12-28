7bb9d51d4fdb71cd07e5d0a17f1b13676dbfc4fa
tvm-albert <albert.thomson@thrithvam.ae>
2015-12-28 15:34:34 +0530

    commiting after fixing gopika download

--- FlyCn/EngineeredDataList/EnggDatalistBaseTable.aspx.designer.cs
+++ FlyCn/EngineeredDataList/EnggDatalistBaseTable.aspx.designer.cs
@@ -355,6 +355,15 @@ namespace FlyCn.EngineeredDataList {
         protected global::System.Web.UI.WebControls.Label Label11;
         
         /// <summary>
+        /// hdfTableName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdfTableName;
+        
+        /// <summary>
         /// hdfFileName control.
         /// </summary>
         /// <remarks>
