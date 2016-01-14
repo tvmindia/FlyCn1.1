efca82e61efef5c3e84d50a1f4e04b63eb994649
tvm-shamila <shamila.tp@thrithvam.me>
2016-01-14 11:18:48 +0530

    security project files

---
+++ FlyCnSecurity/SecurityDAL/SecurityObjects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlyCnSecurity.SecurityDAL
+{
+    public class SecurityObjects
+    {
+
+
+
+    }
+}
