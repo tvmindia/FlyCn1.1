2624f130e402d8e7ba775be27f963f6db8f791ce
tvm-sreejith <sreejith.cv@thrithvam.ae>
2016-01-20 09:50:27 +0530

    changes to debug_#1

--- FlyCn/WebServices/User.asmx.cs
+++ FlyCn/WebServices/User.asmx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
 using System.Web.Services;
 using FlyCn.FlyCnDAL;
 using System.Data.SqlClient;
@@ -18,7 +19,7 @@ namespace FlyCn.WebServices
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class User : System.Web.Services.WebService
     {
         #region User Login
