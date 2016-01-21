6715fee6f77d6c06c9f240f24257e012356fc6cd
tvm-shamila <shamila.tp@thrithvam.me>
2016-01-21 10:37:47 +0530

    missing files added

---
+++ FlyCn/FlycnSecurity/Object.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+
+namespace FlyCn.FlycnSecurity
+{
+    public partial class Object : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadLeftMenu();
+        }
+
+        public void LoadLeftMenu()
+        {
+            UIClasses.InputPages ip = new UIClasses.InputPages();
+            FlyCnDAL.ObjectRegistration objRegstr = new FlyCnDAL.ObjectRegistration();
+            RadTreeView tview = ip.FindLeftTree(this);
+            objRegstr.BindTree(tview);
+            ip.DefaultTreeNode(this, 0);
+
+        }
+    }
+}
